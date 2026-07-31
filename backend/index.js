4e695e78d1b6e15a26b41683b9a97787539a2bfd
Estêvão <estevaoplg@gmail.com>
2026-07-30 23:46:50 -0300

    cria servidor Express inicial com script dev

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORTA = 3000;
+
+app.get('/', (req, res) => {
+    res.json({ mensagem: 'API do Marketplace Economia Circular funcionando!' });
+});
+
+app.listen(PORTA, () => {
+    console.log(`Servidor rodando em http://localhost:${PORTA}`);
+});
